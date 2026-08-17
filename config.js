87d9d07c4564323939fb4dfc2abd1595d8be6633
AlainArce <alainarce39@gmail.com>
2026-08-17 00:35:31 -0300

    Update API keys and URLs in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  TMDB_API_KEY: "",
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  TMDB_API_KEY: "65200b775575f75bf2d009656b3e309a",
+  SUPABASE_URL: "https://rdcwmrvygseisjxhncfd.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_4qscg0J8TiGR8OXaxPXXMQ_-f5NkVJv"
 };
